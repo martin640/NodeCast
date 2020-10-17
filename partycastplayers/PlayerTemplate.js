1aa266ae00d65092b53e0ccea6727a861fadb2ee
Martin <gocrafter.dev@gmail.com>
2020-10-17 18:18:19 +0200

    updated PlayerTemplate.js

--- partycastplayers/PlayerTemplate.js
+++ partycastplayers/PlayerTemplate.js
@@ -1,5 +1,7 @@
 /**
  * This is template for other music players.
+ * Constructor is not called by internal functions so it can contain custom arguments.
+ * For example, default LinuxOmxplayer.js requires volume as argument, which is then passed to called processes.
  */
 module.exports = class {
     constructor() {
@@ -9,17 +11,24 @@ module.exports = class {
     play(file, endCallback) {
         // open and play media file, endCallback should be called when playback ends
     }
+
     pause() {
         // pause if playback is started
     }
+
     resume() {
         // resume if playback is paused
     }
+
     kill() {
         // this function is used to reset music player to initial state
     }
+
     getPosition() {
         // return playback progress in milliseconds
     }
-    isPlaying() { }
+
+    isPlaying() {
+        return this.playing
+    }
 }
