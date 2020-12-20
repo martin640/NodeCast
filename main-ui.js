93170c4ad727a37aee41bd772093ad97f6ddb904
Martin M <34661724+martin640@users.noreply.github.com>
2020-12-20 20:13:16 +0100

    moved files as preparation for react gui

--- main-ui.js
+++ main-ui.js
@@ -10,6 +10,7 @@ function createWindow() {
     const window = new BrowserWindow({
         width: 1200,
         height: 900,
+        frame: false,
         webPreferences: {
             nodeIntegration: true
         },
